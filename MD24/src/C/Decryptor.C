afc5c0b3d2b96d4e67517aed1ba2976e7578b88b
Artemxdd <artjomsfedosovs2@gmail.com>
2024-04-21 10:22:06 +0300

    Refactorinng

--- MD24/src/C/Decryptor.C
+++ MD24/src/C/Decryptor.C
@@ -2,19 +2,19 @@
 
 void Decryptor::decrypt(const std::string& encryptedTxt) {
   for (size_t i{ 0 }; i < encryptedTxt.size(); i += 4) {
-    char encr1{ encryptedTxt[i] }, encr2{ encryptedTxt[i + 1] }, 
+    int encr1{ encryptedTxt[i] }, encr2{ encryptedTxt[i + 1] }, 
       encr3{ encryptedTxt[i + 2] }, encr4{ encryptedTxt[i + 3] };
 
-    char decr1{ (encr1 & 0b1000000) << 1 | encr4 & 0b1000000 | (encr3 & 0b10000) << 1 | (encr2 & 0b1000) << 1 | 
+    int decr1{ (encr1 & 0b1000000) << 1 | encr4 & 0b1000000 | (encr3 & 0b10000) << 1 | (encr2 & 0b1000) << 1 | 
       (encr1 & 0b100) << 1 | encr4 & 0b100 | encr3 & 0b10 | encr2 & 0b1 };
-    char decr2{ (encr2 & 0b1000000) << 1 | (encr1 & 0b10000) << 2 | (encr4 & 0b10000) << 1 | (encr3 & 0b1000) << 1 |
+    int decr2{ (encr2 & 0b1000000) << 1 | (encr1 & 0b10000) << 2 | (encr4 & 0b10000) << 1 | (encr3 & 0b1000) << 1 |
       (encr2 & 0b100) << 1 | (encr1 & 0b10) << 1 | encr4 & 0b10 | encr3 & 0b1 };
-    char decr3{ (encr3 & 0b1000000) << 1 | (encr2 & 0b10000) << 2 | (encr1 & 0b1000) << 2 | (encr4 & 0b1000) << 1 |
+    int decr3{ (encr3 & 0b1000000) << 1 | (encr2 & 0b10000) << 2 | (encr1 & 0b1000) << 2 | (encr4 & 0b1000) << 1 |
       (encr3 & 0b100) << 1 | (encr2 & 0b10) << 1 | (encr1 & 0b1) << 1 | encr4 & 0b1 };
 
-    this->decryptedTxt_.push_back(decr1);
-    this->decryptedTxt_.push_back(decr2);
-    this->decryptedTxt_.push_back(decr3);
+    this->decryptedTxt_.push_back((char)decr1);
+    this->decryptedTxt_.push_back((char)decr2);
+    this->decryptedTxt_.push_back((char)decr3);
   }
 }
 
--- MD24/src/C/Encryptor.C
+++ MD24/src/C/Encryptor.C
@@ -2,15 +2,15 @@
 
 void Encryptor::encrypt(const std::string& decryptedTxt) {
   for (size_t i{ 0 }; i < decryptedTxt.size(); i += 3) {
-    char decr1{ decryptedTxt[i] }, decr2{ decryptedTxt[i + 1] }, decr3{ decryptedTxt[i + 2] };
+    int decr1{ decryptedTxt[i] }, decr2{ decryptedTxt[i + 1] }, decr3{ decryptedTxt[i + 2] };
 
-    char encr1{ (decr1 & 0b10000000) >> 1 | 1 << 5 | (decr2 & 0b1000000) >> 2 | (decr3 & 0b100000) >> 2 | 
+    int encr1{ (decr1 & 0b10000000) >> 1 | 1 << 5 | (decr2 & 0b1000000) >> 2 | (decr3 & 0b100000) >> 2 | 
       (decr1 & 0b1000) >> 1 | (decr2 & 0b100) >> 1 | (decr3 & 0b10) >> 1 };
-    char encr2{ (decr2 & 0b10000000) >> 1 | 1 << 5 | (decr3 & 0b1000000) >> 2 | (decr1 & 0b10000) >> 1 |
+    int encr2{ (decr2 & 0b10000000) >> 1 | 1 << 5 | (decr3 & 0b1000000) >> 2 | (decr1 & 0b10000) >> 1 |
       (decr2 & 0b1000) >> 1 | (decr3 & 0b100) >> 1 | decr1 & 0b1 };
-    char encr3{ (decr3 & 0b10000000) >> 1 | 1 << 5 | (decr1 & 0b100000) >> 1 | (decr2 & 0b10000) >> 1 |
+    int encr3{ (decr3 & 0b10000000) >> 1 | 1 << 5 | (decr1 & 0b100000) >> 1 | (decr2 & 0b10000) >> 1 |
       (decr3 & 0b1000) >> 1 | decr1 & 0b10 | decr2 & 0b1 };
-    char encr4{ decr1 & 0b1000000 | 1 << 5 | (decr2 & 0b100000) >> 1 | (decr3 & 0b10000) >> 1 | decr1 & 0b100 |
+    int encr4{ decr1 & 0b1000000 | 1 << 5 | (decr2 & 0b100000) >> 1 | (decr3 & 0b10000) >> 1 | decr1 & 0b100 |
       decr2 & 0b10 | decr3 & 0b1 };
 
     this->encryptedTxt_.push_back(encr1);
